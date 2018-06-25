cec837b561d57ed14f7c19ee7cc8018093426639
Ian Golden <ian@iangolden.com>
2018-06-25 10:58:13 -0500

    Add in blank scripts

---
+++ before_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Branch: "
+echo $TRAVIS_BRANCH
+
+echo "Tag: "
+echo $TRAVIS_TAG
+
+echo "Commit: "
+echo $TRAVIS_COMMIT
+
+echo "Commit Message: "
+echo $TRAVIS_COMMIT_MESSAGE
+
